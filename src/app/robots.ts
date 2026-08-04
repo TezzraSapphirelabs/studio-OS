552a1eb3c91162f588758651160e411178672d9e
sandeep <i.sando2096@gmail.com>
2026-08-04 15:35:07 +0530

    feat: studio-velonos error fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/'],
     },
-    sitemap: 'https://studio-os.com/sitemap.xml',
+    sitemap: 'https://velonos.dpdns.org/sitemap.xml',
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://studio-os.com',
+      url: 'https://velonos.dpdns.org',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://studio-os.com/login',
+      url: 'https://velonos.dpdns.org/login',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://studio-os.com/login?mode=signup',
+      url: 'https://velonos.dpdns.org/login?mode=signup',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
